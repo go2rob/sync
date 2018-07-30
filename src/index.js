454ed9079573c6653474d7322d939d460d25b260
hariharan888 <hariharan@drylogics.com>
2018-07-30 18:15:34 +0530

    basic counter added

--- src/index.js
+++ src/index.js
@@ -7,23 +7,38 @@
  */
 
 import React, { Component } from "react";
-import { Platform, StyleSheet, Text, View } from "react-native";
-
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+import { Button, Platform, StyleSheet, Text, View } from "react-native";
 
 type Props = {};
 export default class App extends Component<Props> {
+  state = {
+    counter: 0
+  };
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>{"state: " + this.state.counter}</Text>
+        <View style={styles.inLine}>
+          <Button
+            onPress={() =>
+              this.setState(prevState => ({
+                counter: prevState.counter + 1
+              }))
+            }
+            style={styles.button}
+            title="Add"
+          />
+          <Button
+            onPress={() =>
+              this.setState(prevState => ({
+                counter: prevState.counter - 1
+              }))
+            }
+            style={styles.button}
+            title="Subtract"
+          />
+        </View>
       </View>
     );
   }
@@ -41,9 +56,12 @@ const styles = StyleSheet.create({
     textAlign: "center",
     margin: 10
   },
-  instructions: {
-    textAlign: "center",
-    color: "#333333",
-    marginBottom: 5
+  inLine: {
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-around"
+  },
+  button: {
+    width: "30"
   }
 });
